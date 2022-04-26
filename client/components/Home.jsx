e895649d86e29ab1bb7d068e06228ba4375e242b
Seb Ninness <97018698+seb-ninness@users.noreply.github.com>
2022-04-26 21:22:52 +1200

    home page contd

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -12,11 +12,14 @@ function Home() {
   <div className= 'home-container'>
     <div className={city? 'nyc-container' : 'welly-container'}>
       <h3 className='web-dev'>Web Development & Design</h3>
-      <button onClick={toggleCityMode}>{city ? 'WELLY' : 'NYC'} mode</button>
+      <button className='city-button' onClick={toggleCityMode}>{city ? 'WELLY' : 'NYC' } MODE</button>
     </div> 
     <div className='page-holder'>
       <h3>More Content Coming Soon</h3>
     </div>
+    <footer>
+    <a href="mailto:seb.ninness12@gmail.com"> <img src=Images/navigation_icon/email.png"> </a>
+    </footer>
   </div>
   )
 }
